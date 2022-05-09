2ecc86f4981c82959cdf26ecb5a588f301dc4ff8
Valentin <79686793+KubanovValentin@users.noreply.github.com>
2022-05-09 18:22:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+# print("Hello world")
+# x = 3
+# y = 4
+# if x < y:
+#     print("Hello")
+# for i in range(0, 101, 10):
+#     print(i)
+# for i in range(50, -1, -1):
+#     print(i)
+# -------------------------------------------------
+# from PTL import Image, ImageFilter
+
+# before = Image.open("123.jpg")
+# after = before.filter(ImageFilter.BoxBlur(1))
+# after.save("out.jpg")
+# ------------------------------------------------------
+# answer = input("Name?")
+# print(f"hello,{answer}")
+# -----------------------------------------------------
+# import cs50
+# from cs50 import get_int
+# x=get_int("x: ")
+# y=get_int("y: ")
+# ----------------------------------------------------------
+# x = int(input("x:"))
+# y = int(input("y:"))
+# # print(x + y)
+# # print(x / y)
+# if x < y:
+#     print("x меньше y")
+# elif x > y:
+#     print("y меньше x")
+# else:
+#     print("y = x")
+# ---------------------------------------------------------
+# import cs50
+
+# s = cs50.get_string("Вы согласны?")
+# if s == "Да" or s == "да":
+#     print("Вы согласны")
+# elif s == "Нет" or s == "нет":
+#     print("не согласны")
+# -----------------------------------------------------------
+
+# def meow():
+#     print("meow")
+
+# for i in range(3):
+#   meow()
+# --------------------------------------------------------
+# def main():
+#     meow(3)
+
+# def meow(n):
+#     for i in range(n):
+#         print("meow")
+
+# main()
+# -------------------------------------------------------------
+# from cs50 import get_int
+
+# def main():
+#     i = get_positive_int()
+#     print(i)
+# def get_positive_int():
+#   while True:
+#     n=get_positive_int("positive_int")
+#     if n>0:
+#       break
+#   return n
+# -----------------------------------------------------------------
+# for i in range(3):
+#    # print("?", end="")
+#     print("?"*2)
+# print()
+# -----------------------------------------------------------------
+# for i in range(5):
+#   for j in range(3):
+#     print("?", end="")
+#   print()  
+# --------------------------------------------------------------------
